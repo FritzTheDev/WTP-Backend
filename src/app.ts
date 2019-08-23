47484e51ec955cdea89f1c53bc95e8add0f85e2d
ottovbismarck <fritzj96@gmail.com>
2019-08-22 23:36:21 -0700

    installed cors package & type

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,24 @@
+import { config } from "dotenv";
 import express, { json } from "express";
+import Mongoose from "mongoose";
+
+// Environment Var Injection
+config();
+
+// Throws if no Mongo URI variable is set
+if (!process.env.MONGO_URI) {
+  throw new Error("Mongo URI Environment Variable Not Set");
+}
+
+// Connect To Mongo
+Mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+Mongoose.connection.on("connected", () => {
+  console.log("Mongoose Connection Active");
+});
+
+Mongoose.connection.on("error", (err) => {
+  console.error("Database Error: " + err);
+});
 
 // Instantiate App
 const app: express.Application = express();
